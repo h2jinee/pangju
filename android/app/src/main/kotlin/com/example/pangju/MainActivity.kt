d1b8b1284d5b69abafcf920ea06273e9ba3be6e3
h2jinee <wjsgmlwls97@gmail.com>
2024-06-05 16:09:46 +0900

    feat/네이버 지도 API 연동

--- android/app/src/main/kotlin/com/example/pangju/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pangju/MainActivity.kt
@@ -1,5 +1,44 @@
-package com.example.pangju
+package com.pangju
 
+import android.os.Bundle
+import android.content.pm.PackageManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.pangju/meta"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "getClientId") {
+                val clientId = getClientId()
+                if (clientId != null) {
+                    result.success(clientId)
+                } else {
+                    result.error("UNAVAILABLE", "Client ID not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getClientId(): String? {
+        return try {
+            val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+            val bundle = appInfo.metaData
+            bundle.getString("com.naver.maps.map.CLIENT_ID")
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
--- lib/screens/location/location_screen.dart
+++ lib/screens/location/location_screen.dart
@@ -1,6 +1,47 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pangju/widgets/bottom_navigation_bar.dart';
+
+void main() async {
+  await _initialize();
+  runApp(const NaverMapApp());
+}
+
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String clientId = await _getClientId();
+  await NaverMapSdk.instance.initialize(
+    clientId: clientId, // 클라이언트 ID 설정
+    onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed"),
+  );
+}
+
+Future<String> _getClientId() async {
+  const platform = MethodChannel('com.pangju/meta');
+  try {
+    final String clientId = await platform.invokeMethod('getClientId');
+    return clientId;
+  } on PlatformException catch (e) {
+    log("Failed to get client ID: '${e.message}'.");
+    return '';
+  }
+}
+
+class NaverMapApp extends StatelessWidget {
+  const NaverMapApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LocationScreen(),
+    );
+  }
+}
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -11,6 +52,43 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   int _selectedIndex = 1; // 해당 페이지의 인덱스
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+  bool _isMapSdkInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<void> _initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    String clientId = await _getClientId();
+    await NaverMapSdk.instance.initialize(
+      clientId: clientId, // 클라이언트 ID 설정
+      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed"),
+    );
+    setState(() {
+      _isMapSdkInitialized = true;
+    });
+  }
+
+  Future<String> _getClientId() async {
+    const platform = MethodChannel('com.pangju/meta');
+    try {
+      final String clientId = await platform.invokeMethod('getClientId');
+      return clientId;
+    } on PlatformException catch (e) {
+      log("Failed to get client ID: '${e.message}'.");
+      return '';
+    }
+  }
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,19 +124,36 @@ class _LocationScreenState extends State<LocationScreen> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('내 근처 화면입니다.'),
-      ),
+      body: _isMapSdkInitialized
+          ? Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: NaverMap(
+                    options: const NaverMapViewOptions(
+                      indoorEnable: true, // 실내 맵 사용 가능 여부 설정
+                      locationButtonEnable: false, // 위치 버튼 표시 여부 설정
+                      consumeSymbolTapEvents: false, // 심볼 탭 이벤트 소비 여부 설정
+                    ),
+                    onMapReady: (controller) async {
+                      // 지도 준비 완료 시 호출되는 콜백 함수
+                      mapControllerCompleter
+                          .complete(controller); // Completer에 지도 컨트롤러 완료 신호 전송
+                      log("onMapReady", name: "onMapReady");
+                    },
+                  ),
+                ),
+              ],
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.white,
         selectedItemColor: const Color(0xFF37A3E0),
         unselectedItemColor: const Color(0xFF484848),
-        onTap: (index) => onItemTapped(context, index, (int idx) {
-          setState(() {
-            _selectedIndex = idx;
-          });
-        }),
+        onTap: (index) => onItemTapped(index),
         currentIndex: _selectedIndex,
         items: bottomNavigationBarItems(context, _selectedIndex, (int idx) {
           setState(() {
